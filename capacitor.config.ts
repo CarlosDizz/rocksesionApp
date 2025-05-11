1b22ce2924bb5048dea1318c1ba2bcf67aa395d8
Carlos <dizz01@gmail.com>
2025-05-11 08:57:48 +0200

    init: rocksessions app base

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.rocksessions',
+  appName: 'rocksessions-app',
+  webDir: 'www'
+};
+
+export default config;
