3e0c38c92f3f49a865e085cd484551777f2edb2f
Carlos <dizz01@gmail.com>
2025-05-19 22:46:30 +0200

    Fix login and register page

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.rocksessions',
   appName: 'rocksessions-app',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    Keyboard: {
+      resize: 'body'  // o 'none' si aún falla, depende de tu layout
+    }
+  }
 };
 
 export default config;
